38fc8af3465d9dba4763dda26f47ce7ad8ef072e
Seu Nome <seuemail@example.com>
2025-04-07 19:28:52 -0300

    adição de novos arquivos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import express from "express";
+
+import mongoose from "mongoose";
+
 import routes from "./routes";
 
 class App {
   constructor() {
     this.server = express();
+
+    mongoose.connect(
+      "mongodb+srv://devhouse:devhouse@cluster0.yifjzbl.mongodb.net/devhouse?retryWrites=true&w=majority&appName=Cluster0"
+    );
 
     this.middlewares();
     this.routes();
